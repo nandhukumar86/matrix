3fdc5c7f796fc66f8c036273a6c1d088e5ed5d79
Nandhakumar <nandhu_kumar86@yahoo.com>
2025-03-14 11:35:58 +0530

    Matrix operations added

--- MatrixCalc/MatrixOperations.cs
+++ MatrixCalc/MatrixOperations.cs
@@ -93,5 +93,63 @@ namespace MatrixCalc
 
             return result;
         }
+
+        public static int[,] CustomAddition(int[,] matrixA, int[,] matrixB, int[,] matrixC)
+        {
+            int rowsA = matrixA.GetLength(0);
+            int colsA = matrixA.GetLength(1);
+            int rowsB = matrixB.GetLength(0);
+            int colsB = matrixB.GetLength(1);
+            int rowsC = matrixC.GetLength(0);
+            int colsC = matrixC.GetLength(1);
+
+            if (rowsA != rowsB || rowsA != rowsC || colsA != colsB || colsA != colsC)
+            {
+            throw new ArgumentException("All matrices must have the same dimensions.");
+            }
+
+            int[,] result = new int[rowsA, colsA];
+
+            for (int i = 0; i < rowsA; i++)
+            {
+                for (int j = 0; j < colsA; j++)
+                {
+                    int x = i + j; // Example of varying x
+                    int y = i - j; // Example of varying y
+                    result[i, j] = (x * matrixA[i, j]) + (y * (matrixB[i, j] + matrixC[i, j]));
+                }
+            }
+
+            return result;
+        }
+        
+        public static int[,] ColumnOperations(int[,] matrix, int[][] columnOperations)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = columnOperations.Length;
+
+            int[,] result = new int[rows, cols];
+
+            for (int j = 0; j < cols; j++)
+            {
+                for (int i = 0; i < rows; i++)
+                {
+                    result[i, j] = 0;
+                    foreach (int index in columnOperations[j])
+                    {
+                        if (index >= 0 && index < matrix.GetLength(1))
+                        {
+                            result[i, j] += matrix[i, index];
+                        }
+                        else
+                        {
+                            throw new ArgumentException("Index out of bounds in column operations.");
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
     }
 }
